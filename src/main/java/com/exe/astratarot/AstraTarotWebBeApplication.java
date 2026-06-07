c2d92c08fcc1add2068c2136ef5b6e6730c8affb
tugnngw <tugnngw@gmail.com>
2026-06-07 14:25:44 +0700

    Start project

---
+++ src/main/java/com/exe/astratarot/AstraTarotWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.exe.astratarot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstraTarotWebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AstraTarotWebBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exe/astratarot/AstraTarotWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.astratarot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstraTarotWebBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
